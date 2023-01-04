f046db86b7c79dd5f186214f1e48500d1309906d
Lizzie <lizziewrab@me.com>
2023-01-04 15:17:10 -0600

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
     
     has_many :user_activities 
     has_many :activities, through: :user_activities
+
+    validates :email, presence: true, uniqueness: true
+    validates :first_name, presence: true, length: {maximum 15}
+    validates :last, presence: true, length: {maximum 20}
+    validates :bio, presence: true, length: {maximum 300}
+    validates :question, presence: true, length: {maximum 300}
+
 end
