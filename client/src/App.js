0c3c30cd006a5c2fe30ab46bef55cbaab5314296
Lizzie <lizziewrab@me.com>
2023-01-05 16:07:48 -0600

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+// import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    fetch('/activities')
+    .then((r) => r.json())
+    .then(activities => setActivities(activities));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {activities.map((activity) => 
+      <h2>{activity.name}</h2>
+      )}
     </div>
   );
 }
