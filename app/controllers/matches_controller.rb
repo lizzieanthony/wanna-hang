5a0a832afd9892e64d75cbe77a70b8f733100c5c
Lizzie <lizziewrab@me.com>
2023-02-18 19:06:59 -0600

    matches controller methods written

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,19 @@
 class MatchesController < ApplicationController
+
+    def index 
+        matches = current_user.matches.all 
+        render json: matches, status: 200
+    end
+
+    def create
+        match = current_user.matches.create(match_params)
+        render json: matches, status: 200
+    end
+
+    private
+
+    def match_params
+        params.permit(:user_id, :user2_id)
+    end
+
 end
